f426f9b8ec3e49f985cf996286d814200e0f1c1e
Somay <somaychauhan98@gmail.com>
2023-07-20 12:55:51 +0530

    changes

--- src/data/siteMetadata.js
+++ src/data/siteMetadata.js
@@ -6,17 +6,11 @@ const siteMetadata = {
   language: 'en-us',
   theme: 'dark', // system, dark or light
   siteUrl: 'https://dalelarroder.com',
-  siteRepo: 'https://github.com/dlarroder/dalelarroder-blog',
+  siteRepo: 'https://github.com/SomayChauhan/somay-chauhan',
   siteLogo: '/static/images/logo.png',
-  image: '/static/images/avatar.webp',
-  socialBanner: '/static/images/twitter-card.png',
   email: 'somaychauhan98@gmail.com',
-  github: 'https://github.com/dlarroder',
-  twitter: 'https://twitter.com/dalelarroder',
-  facebook: 'https://facebook.com/dlarroder',
+  github: 'https://github.com/SomayChauhan',
   linkedin: 'https://www.linkedin.com/in/somay-chauhan-20a73115b/',
-  spotify: 'https://open.spotify.com/user/12162121994?si=e685b3546f414967',
-  steam: 'https://steamcommunity.com/id/dlarroder/',
   locale: 'en-US',
 };
 
